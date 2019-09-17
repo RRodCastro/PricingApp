64602c722e43f461f792091066d16ef181d43a46
Rodrigo <mrcastro9574@gmail.com>
2019-09-17 15:58:57 -0600

    Refactor settings screen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,85 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ExpoConfigView } from '@expo/samples';
+import { SectionList, Image, StyleSheet, Text, View } from 'react-native';
 
-export default function SettingsScreen() {
+export default class SettingsScreen extends Component {
   /**
    * Go ahead and delete ExpoConfigView and replace it with your content;
    * we just wanted to give you a quick view of your config.
    */
-  return <ExpoConfigView />;
+  render() {
+    return (
+      <ExpoConfigView />
+    )
+  }
 }
 
 SettingsScreen.navigationOptions = {
   title: 'app.json',
 };
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  titleContainer: {
+    paddingHorizontal: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
+    flexDirection: 'row',
+  },
+  titleIconContainer: {
+    marginRight: 15,
+    paddingTop: 2,
+  },
+  sectionHeaderContainer: {
+    backgroundColor: '#fbfbfb',
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#ededed',
+  },
+  sectionHeaderText: {
+    fontSize: 14,
+  },
+  sectionContentContainer: {
+    paddingTop: 8,
+    paddingBottom: 12,
+    paddingHorizontal: 15,
+  },
+  sectionContentText: {
+    color: '#808080',
+    fontSize: 14,
+  },
+  nameText: {
+    fontWeight: '600',
+    fontSize: 18,
+  },
+  slugText: {
+    color: '#a39f9f',
+    fontSize: 14,
+    backgroundColor: 'transparent',
+  },
+  descriptionText: {
+    fontSize: 14,
+    marginTop: 6,
+    color: '#4d4d4d',
+  },
+  colorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  colorPreview: {
+    width: 17,
+    height: 17,
+    borderRadius: 2,
+    marginRight: 6,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#ccc',
+  },
+  colorTextContainer: {
+    flex: 1,
+  },
+});
