8a1f735743c8c3ed98f83b140df6c3d0fcd19102
Rodrigo <mrcastro9574@gmail.com>
2019-10-29 15:13:04 -0600

    Add auto complete component to form

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,15 +9,84 @@ import {
   View
 } from 'react-native';
 
+import Autocomplete from './Autocomplete';
+
 import { Button, Input, CheckBox } from 'react-native-elements'
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { films } from '../assets/data/products'
 
 export default class HomeScreen extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      films: films,
+      query: '',
+      selectedItem: false
+    }
+  }
 
+  renderFilm(film) {
+    return (
+      <View>
+        {film.map((item, index) => (
+          <Text key={index} onPress={() => {this.setState({query: `${item.key}, ${item.title}`, selectedItem: true }) }} style={styles.titleText}> {item.key}, {item.title} </Text>
+        ))}
+      </View>
+    );
+  }
+
+  findFilm(query) {
+    if (query === '' | !Boolean(query)) {
+      return [];
+    }
+    const { films } = this.state;
+    const regex = new RegExp(`^${query}`, 'i');
+    return films.filter(film =>  film.title.search(regex) >= 0 | film.key.search(regex) >= 0);
+  }
+
+  renderSearch () {
+    const { query } = this.state;
+    const films = this.findFilm(query);
+    const comp = (a, b) => a === b;
+    
+    return (
+    <View style={styles.container2}>
+      <Autocomplete
+        hideResults={query.length == 0}
+        autoCapitalize="none"
+        autoCorrect={false}
+        containerStyle={styles.autocompleteContainer}
+        data={films.length === 1 && comp(query, films[0].key) ? [] : films}
+        defaultValue={query}
+        onChangeText={text => { this.setState({ query: text, selectedItem: false })}}
+        placeholder="Ingresar producto"
+        renderItem={({ title, key }) => { (
+          <Text>
+            {key }
+          </Text>
+        )}
+        }
+      />
+      {films.length > 0 && !this.state.selectedItem  && 
+      (<View style={styles.descriptionContainer}>
+        {films.length > 0 ? (
+          this.renderFilm(films.slice(0, 5))
+        ) : (
+          <View/>
+        )}
+      </View>)
+      }
+      
+    </View>
+  )
+}
+  
   renderForm() {
     const { screenProps } = this.props
+    
     return (
-
+      <View>
       <View style={styles.getStartedContainer}>
         <View style={{ margin: 10 }}>
           <Input
@@ -28,11 +97,11 @@ export default class HomeScreen extends Component {
             fontWeight='Bold'
             errorMessage={''}
             placeholder={''}
-            inputStyle={{color: 'white'}}
+            inputStyle={{color: 'white', display: "none", borderBottomWidth: 0}}
           >
           </Input>
+          {this.renderSearch()}
         </View>
-
         <View style={{ margin: 10 }}>
           <Input
             keyboardType='numeric'
@@ -118,6 +187,8 @@ export default class HomeScreen extends Component {
           buttonStyle={{width: 120}}
         />
         }
+      </View>
+
       </View>
     )
   }
@@ -266,4 +337,43 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#2e78b7',
   },
+
+  container2: {
+    backgroundColor: 'transparent',
+    flex: 0,
+    paddingTop: 15,
+    paddingBottom: 20,
+    zIndex: 99999
+  },
+  autocompleteContainer: {
+    flex: 1,
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    zIndex: 1
+  },
+  itemText: {
+    fontSize: 12,
+    margin: 2
+  },
+  descriptionContainer: {
+    // `backgroundColor` needs to be set otherwise the
+    // autocomplete input will disappear on text input.
+    backgroundColor: '#F5FCFF',
+    marginTop: 25
+  },
+  infoText: {
+    textAlign: 'center'
+  },
+  titleText: {
+    fontSize: 12,
+    fontWeight: '500',
+    marginBottom: 10,
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  openingText: {
+    textAlign: 'center'
+  }
 });
